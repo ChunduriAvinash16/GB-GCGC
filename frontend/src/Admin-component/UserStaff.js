827f8312c47c2412662a3296f3a081220a4b5f37
Avinash <chunduriaviansh6@gmail.com>
2020-09-07 10:23:58 +0530

    Added user-staff and userstudent to sidebar  in Admin page

---
+++ frontend/src/Admin-component/UserStaff.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserStaff() {
+    return (
+        <div>
+            Staff
+        </div>
+    )
+}
+
+export default UserStaff;
---
+++ frontend/src/Admin-component/UserStudent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserStudent() {
+    return (
+        <div>
+         Student   
+        </div>
+    )
+}
+
+export default UserStudent
