5614e591c215f4d5bfc211d66f086dbb68affb06
Avinash <chunduriaviansh6@gmail.com>
2020-09-17 11:50:05 +0530

    Admin Page Placement Not working

--- Admin-backend/PlacementsDashboard.php
+++ Admin-backend/PlacementsDashboard.php
@@ -3,9 +3,9 @@ require_once 'auth.php';
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-$year =$_GET["year"];
+$year =$_GET['year'];
 $students = [];
-$sql = "select * from placements_dash where YOP=$year limit 5";
+$sql = "SELECT `placements_dash`.`Date`,`companies`.`Company_name`,`placements_dash`.`Date`,`placements_dash`.`CTC` FROM `placements_dash`,`companies` WHERE `placements_dash`.`cid`=`companies`.`company_id` and `placements_dash`.`YOP`=$year limit 5";
 $status="";
 $ct=0;
 if($result=mysqli_query($con,$sql)){
